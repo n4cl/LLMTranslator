1388e0ca2d7a86878c48522f94b2d54d5c7618f4
n4cl <devn4cl@gmail.com>
2023-08-13 00:27:21 +0900

    Add: 翻訳処理を画面に追加

--- src/llm_translation.py
+++ src/llm_translation.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import random
+from llm_translator import Translator
+
 
 def select_paramater():
     model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
@@ -18,29 +20,35 @@ def main():
 
     # モデルの選択
     model_name, temperature = select_paramater()
+    llm = Translator(model=model_name, temperature=temperature)
 
-    # レイアウトを横に並べるためのコンテナを作成
     col1, col2 = st.columns(2)
 
     # 左側のテキストエリアを配置
     with col1:
         source_lang = st.selectbox("Source Language", ("English", "Japanese"))
-        source_text = st.text_area("原文", height=300)
 
     # 右側のテキストエリアを配置
     with col2:
         target_lang = st.selectbox("Target Language", ("English", "Japanese"))
 
-        if source_text:
-                target_text = st.text_area("訳文", value=source_text, height=300)
-        else:
-            target_text = st.text_area("訳文", height=300)
+    source_text = st.text_area("Input", height=300)
+
+    translation = None
+    if st.button("Translate"):
+        with st.spinner("Translating ..."):
+            translation = llm.translate_and_get_cost(source_language=source_lang, target_language=target_lang, text=source_text)
+            if translation.is_success and translation.cost > 0:
+                st.write(translation.translated_text)
+            else:
+                st.write("Failed to translate.")
 
     st.sidebar.markdown("## Costs")
     st.sidebar.markdown("**Total cost**")
-    # TODO: 仮実装
-    r = random.uniform(1, 100)
-    st.sidebar.markdown(f"- ${r}")
+    if translation and translation.is_success and translation.cost > 0:
+        st.sidebar.markdown(f"- ${translation.cost}")
+    else:
+        st.sidebar.markdown("- $0")
 
 if __name__ == "__main__":
     main()
