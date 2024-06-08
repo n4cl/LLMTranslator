ad80bfc1faff433473eb38919c74802b19da1c04
n4cl <devn4cl@gmail.com>
2024-06-09 00:06:04 +0900

    Update: モデルの更新

--- src/component_template.py
+++ src/component_template.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 def generate_default_paramater():
-    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"), horizontal=True)
+    model = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4o"), horizontal=True)
     if model == "GPT-3.5":
         model_name = "gpt-3.5-turbo-0125"
-        max_chars = 3000
+
     else:
-        model_name = "gpt-4-0125-preview"
-        max_chars = 7000
+        model_name = "gpt-4o-2024-05-13"
+    max_chars = 10000
     temperature = st.sidebar.slider("Temperature:", min_value=0.0, max_value=2.0, value=0.0, step=0.1)
 
     return model_name, max_chars, temperature
